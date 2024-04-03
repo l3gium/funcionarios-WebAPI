47b43073dd3f9e319e0d0dfb76b694446b994e9a
l3gium <miguel.luiztto@gmail.com>
2024-04-03 10:07:16 -0300

    conexão com banco de dados funcionando

---
+++ funcionarios-WebAPI/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using funcionarios_WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace funcionarios_WebAPI.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+        public DbSet<FuncionarioModel> Funcionarios { get; set; }
+    }
+}
